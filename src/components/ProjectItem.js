158667e06e91ca868f4c97b2db823235f0cbeda3
Your Name <youremail@example.com>
2023-08-02 12:49:08 +0300

    commit message

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -5,8 +5,9 @@ function ProjectItem({ name, about, technologies }) {
     <div className="project-item">
       <h3>{name}</h3>
       <p>{about}</p>
-      <div className="technologies">
-        {/* render a <span> for each technology in the technologies array */}
+      <div className="technologies">{/* render a <span> for each technology in the technologies array */
+        technologies.map(technology => <span key={technology}>{technology}</span>)
+        }
       </div>
     </div>
   );
